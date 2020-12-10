747ab8eb30d424f4cd13bf6358f9ea554be05cf4
Steven M. Cohn <steven_cohn@waters.com>
2020-12-10 10:18:47 -0500

    poc

--- Colorizer/Program.cs
+++ Colorizer/Program.cs
@@ -12,13 +12,36 @@ namespace Colorizer
 	{
 		static void Main(string[] args)
 		{
+			Foo();
+			CSharp();
+		}
+
+
+		static void Foo()
+		{
+			Console.WriteLine();
+			Console.WriteLine("foo------------------------------");
+
 			var colorizer = new Colorizer("foo");
 
 			var root = colorizer.Colorize("header\n\tfoo 123\n   // blah\nfooter");
 			Console.WriteLine(root.ToString());
 
-
 			var one = colorizer.ColorizeOne("foo\n123");
+			Console.WriteLine(one);
+		}
+
+
+		static void CSharp()
+		{
+			Console.WriteLine();
+			Console.WriteLine("csharp---------------------------");
+
+			var colorizer = new Colorizer("csharp");
+
+			var one = colorizer.ColorizeOne(
+				"public void Foobar(int arg)\n{\n    var s = \"bubble\";\n}");
+
 			Console.WriteLine(one);
 		}
 	}
